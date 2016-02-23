041d133c7e2bb10f19ea7e64aad2dea39265bb0c
Breiden Busch <breiden.busch@gmail.com>
2016-02-23 14:17:56 -0700

    Finished Step 4

--- lessonHider.js
+++ lessonHider.js
@@ -1,7 +1,13 @@
 angular.module('directivePractice').directive('lessonHider', function() {
   return {
     restrict: 'E',
-    templateUrl: 'lessonHider.html'
+    templateUrl: 'lessonHider.html',
+    scope: {
+      lesson: '='
+    },
+    link: function(scope, element, attributes) {
+
+    }
   }
 
 
